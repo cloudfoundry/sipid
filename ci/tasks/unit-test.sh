19c484a37e57d9b9481e074f2308cbed61d0329b
Christopher Brown <cbrown@pivotal.io>
2017-03-03 11:46:27 -0800

    Get gomega for unit tests
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

--- ci/tasks/unit-test.sh
+++ ci/tasks/unit-test.sh
@@ -5,6 +5,7 @@ set -e
 export GOPATH="${PWD}/gopath"
 export PATH="${GOPATH}/bin:${PATH}"
 go get github.com/onsi/ginkgo/ginkgo
+go get github.com/onsi/ginkgo/gomega
 
 cd gopath/src/github.com/cloudfoundry/sipid
 
